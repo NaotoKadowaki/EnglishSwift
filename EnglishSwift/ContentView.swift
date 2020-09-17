231a01a67366bec740190de78cf34036bf4a6801
NaotoKadowaki <vivi0601naoto@icloud.com>
2020-09-17 12:10:24 +0900

    new

---
+++ EnglishSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EnglishSwift
+//
+//  Created by NAOTO KADOWAKI on 2020/09/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EnglishSwift/EnglishSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishSwiftApp.swift
+//  EnglishSwift
+//
+//  Created by NAOTO KADOWAKI on 2020/09/17.
+//
+
+import SwiftUI
+
+@main
+struct EnglishSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
